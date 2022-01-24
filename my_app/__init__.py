20a0937aec6a6d39b8fc0420016a8b8b1ab1d7a5
Junaid Awan <awanj@cardiff.ac.uk>
2022-01-24 18:27:46 +0000

    commit

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -4,13 +4,22 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_manager
 
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__)
 
 app.config["SECRET_KEY"] = '9eaead86f0682ed25b8b86f29871dcf9d44d8d53ef86a981sssssssss33333333334343443'
 
-basedir=os.path.abspath(os.path.dirname(__file__))
+#basedir=os.path.abspath(os.path.dirname(__file__))
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'my_database.db')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'my_database.db')
+#connecting to databse
+username = "c21100637"
+password = "passW0rddd"
+db_name = "flask_coursework"
+
+app_config_sql_db_uri = f"mysql+pymysql://{username}:{password}@csmysql.cs.cf.ac.uk:3306/{username}_{db_name}"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = app_config_sql_db_uri
+
 
 db = SQLAlchemy(app)
 
